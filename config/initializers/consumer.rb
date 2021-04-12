f543b84c24eddc069a3ce652233bed1551b693f1
avbelyshev <avbelishev@gmail.com>
2021-04-13 01:06:38 +0300

    Add auth result logging

--- config/initializers/consumer.rb
+++ config/initializers/consumer.rb
@@ -5,9 +5,20 @@ queue = channel.queue('auth', durable: true)
 queue.subscribe(manual_ack: true) do |delivery_info, properties, payload|
   payload = JSON(payload)
 
-  result = AuthService.call(payload['token'])
+  token = payload['token']
+  result = AuthService.call(token)
 
   user_id = result.present? ? result.user_id : ''
+
+  if user_id.present?
+    Application.logger.info(
+      'Verified token',
+      token: token,
+      user_id: user_id
+    )
+  else
+    Application.logger.warn('User not found', token: token)
+  end
 
   exchange.publish(
     user_id.to_s,
